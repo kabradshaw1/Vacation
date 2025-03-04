116ea4e005245899cc903e1f004f0853182c1658
Kyle Bradshaw <itllbok82@gmail.com>
2025-03-04 09:23:19 -0600

    working

--- lib/main.dart
+++ lib/main.dart
@@ -14,42 +14,18 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.deepPurple,
           brightness: Brightness.dark,
         ),
       ),
-      home: const MyHomePage(title: 'Is flutter better than react native?'),
+      home: const MyHomePage(title: 'Is Flutter better than React Native?'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -74,19 +50,26 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
             Container(
-              height: 150,
-              width: 150,
+              height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.all(50),
+              padding: EdgeInsets.all(50),
               decoration: BoxDecoration(
                 color: Colors.teal,
                 borderRadius: BorderRadius.circular(25.0),
               ),
-
-              child: const Text(
-                'Hello bitches',
-
-                style: TextStyle(color: Colors.cyan),
+            ),
+            Container(
+              height: 150,
+              width: double.infinity,
+              margin: EdgeInsets.all(50),
+              padding: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.teal,
+                borderRadius: BorderRadius.circular(25.0),
               ),
             ),
           ],
